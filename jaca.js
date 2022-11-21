8c7d49399abea6eded58193fef4af31473795be2
FelipeBiver <12100757@aluno.cotemig.com.br>
2022-11-20 22:13:39 -0300

    final

---
+++ jaca.js
@@ -0,0 +1,25 @@
+acao();
+function acao(){
+ 
+let img =document.querySelectorAll('.box-image img')
+setInterval(function(){
+ 
+img[interval].style.display = 'none';
+interval++
+if(interval > maxSlider){
+    interval=0
+}
+img[interval].style.display = 'block'
+},8000)
+ 
+ 
+}
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+    document.getElementById(el).style.display = 'block';
+    else
+    document.getElementById(el).style.display = 'none';
+}
+var interval=0;
+var maxSlider = document.querySelectorAll('.box-image').length -1;
